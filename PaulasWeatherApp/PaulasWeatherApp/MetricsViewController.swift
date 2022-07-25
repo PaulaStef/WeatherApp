912a98f223358a17b911f5f9ed700056cd01bdb3
Stef, Paula <>
2022-07-25 16:09:13 +0300

    Configured the tab bar with 3 tabs

---
+++ PaulasWeatherApp/PaulasWeatherApp/MetricsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MetricsViewController.swift
+//  PaulasWeatherApp
+//
+//  Created by Stef, Paula on 7/25/22.
+//
+
+import UIKit
+
+class MetricsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemYellow
+    }
+}
---
+++ PaulasWeatherApp/PaulasWeatherApp/WeatherViewController.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherViewController.swift
+//  PaulasWeatherApp
+//
+//  Created by Stef, Paula on 7/25/22.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemRed
+    }
+}
